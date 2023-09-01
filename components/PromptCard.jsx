1d9de6a71c005b464475c10f0377ea21f1434a8f
Rajamiththiran <rjmithu7@gmail.com>
2023-09-01 23:50:38 +0530

    updated

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,5 +1,30 @@
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  console.log(post.creator.image);
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center cursor-pointer gap-3">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3>hello</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PromptCard;

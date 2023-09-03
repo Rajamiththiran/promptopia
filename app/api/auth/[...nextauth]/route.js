f520becd6241964a86d4c4b7d5a31bde9e25f999
Rajamiththiran <rjmithu7@gmail.com>
2023-09-04 02:12:59 +0530

    u

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import User from "@models/user";
 import { connectToDb } from "@utils/database";
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
+
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
@@ -9,10 +9,6 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
 
